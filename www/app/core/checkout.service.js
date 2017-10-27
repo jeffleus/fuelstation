b03b7bce44616a813960884a5c3b1712ef636ce6
Jeff Leininger <jeffleus@gmail.com>
2017-10-27 16:24:02 -0700

    update checkout svc to use monthly item report
    was using the mock version, so needed to supply the year/month in the
    url params

--- www/app/core/checkout.service.js
+++ www/app/core/checkout.service.js
@@ -83,9 +83,11 @@
         */
         function _getMonthCounts(timePeriod){
             //return $resource(ApiEndpoint.url + 'Checkouts/GetMonthCounts/Month/' + timePeriod.month + '/Year/' + timePeriod.year);
+            var reportUrl = 'https://f5ekrwo1b6.execute-api.us-west-2.amazonaws.com/dev/report/checkouts/monthly';
+            reportUrl += '/' + timePeriod.year + '/' + timePeriod.month;
             return {
                 query: function(){
-                    return $http.get('https://f5ekrwo1b6.execute-api.us-west-2.amazonaws.com/dev/report/checkouts/monthly')
+                    return $http.get(reportUrl)
                         .then(function(result){
                             return result.data;
                         });
