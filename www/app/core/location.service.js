46cf8a96730f949f51fe74fbebd414b2e6478443
jeffleus <jeffleus@gmail.com>
2017-10-30 07:39:19 -0700

    Set unique ids on dummy location data

--- www/app/core/location.service.js
+++ www/app/core/location.service.js
@@ -15,8 +15,8 @@
         function _init() {
 			self.locations = [
 				{LocationID: 1, title: 'Fuel Station', description: 'main fuel station location'},
-				{LocationID: 1, title: 'Grab-N-Go', description: 'self-service snack-only location'},
-				{LocationID: 1, title: 'Acosta Center', description: 'supplemental snack location'}
+				{LocationID: 2, title: 'Grab-N-Go', description: 'self-service snack-only location'},
+				{LocationID: 3, title: 'Acosta Center', description: 'supplemental snack location'}
 			];
         }
         
