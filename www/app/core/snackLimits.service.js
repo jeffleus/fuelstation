7edc5227323122e6a0143e715d45d492f54c25b9
jeffleus <jeffleus@gmail.com>
2017-11-14 14:13:19 -0800

    Update limit-svc for prod-vatech branch

--- www/app/core/snackLimits.service.js
+++ www/app/core/snackLimits.service.js
@@ -12,7 +12,7 @@
         //mocked service url
         //var url = 'https://n6jjrfl2j7.execute-api.us-west-2.amazonaws.com/dev/snacklimits/';
         //live service url
-        var url = 'https://fliw63osxc.execute-api.us-west-2.amazonaws.com/dev/snacklimits/';
+        var url = 'https://t40iqtfss1.execute-api.us-west-2.amazonaws.com/dev/snacklimits/';
         
         return service;
         
