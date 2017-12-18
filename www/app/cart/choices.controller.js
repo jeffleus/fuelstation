dc332b613c3a265fe4ee1eeb2ece841ec681163a
jeffleus <jeffleus@gmail.com>
2017-12-18 11:44:44 -0800

    order override added when limit reached
    added a check for limit exceed for items w/ value >1
    (e.g. total @ 4 prevent ordering 2pt item if limit is 5)
    And provided a general override button for allowing this.

--- www/app/cart/choices.controller.js
+++ www/app/cart/choices.controller.js
@@ -3,7 +3,7 @@
 
     angular.module('app.studentChoices', [])
 
-    .controller('StudentChoicesCtrl', function (ChoiceSvc, AccountSvc, OrderSvc, $state, CheckoutSvc, $ionicSideMenuDelegate, $ionicPopup, IonicAlertSvc, $interval, $scope) {
+    .controller('StudentChoicesCtrl', function (ChoiceSvc, AccountSvc, OrderSvc, $state, CheckoutSvc, $ionicSideMenuDelegate, $ionicPopup, IonicAlertSvc, $interval, $scope, $q) {
         var vm = this;
 
         var timer;
@@ -11,7 +11,7 @@
         vm.accountSvc = AccountSvc;
         vm.choiceSvc = ChoiceSvc;
         vm.orderItems = OrderSvc.orderItems;
-        vm.timeRemaining = 120;
+//        vm.timeRemaining = 120;
 		vm.categories = vm.choiceSvc.categories();
 
         vm.cancel = _cancel;
@@ -24,22 +24,22 @@
 
         function init() {
 			console.log('Categorical Choices', vm.categoricalChoices);
-            // Start counting down timer, which was initialized to 120 above
-            timer = $interval(function () {
-                vm.timeRemaining--;
-                vm.seconds = vm.timeRemaining < 10 ? "0" + vm.timeRemaining : vm.timeRemaining;
-
-                if (vm.timeRemaining === 0) {
-                    $interval.cancel(timer);
-
-                    var opts = {};
-                    opts.title = "You have been logged out due to inactivity.";
-                    opts.shouldClearID = true;
-                    opts.redirect = 'tab.orderList';
-
-                    IonicAlertSvc.alert(opts);
-                }
-            }, 1000);
+//            // Start counting down timer, which was initialized to 120 above
+//            timer = $interval(function () {
+//                vm.timeRemaining--;
+//                vm.seconds = vm.timeRemaining < 10 ? "0" + vm.timeRemaining : vm.timeRemaining;
+//
+//                if (vm.timeRemaining === 0) {
+//                    $interval.cancel(timer);
+//
+//                    var opts = {};
+//                    opts.title = "You have been logged out due to inactivity.";
+//                    opts.shouldClearID = true;
+//                    opts.redirect = 'tab.orderList';
+//
+//                    IonicAlertSvc.alert(opts);
+//                }
+//            }, 1000);
         }
 
         // Cancel timer when controller is destroyed (i.e. when route changes), otherwise timer continues even if user has already been logged out
@@ -62,7 +62,6 @@
         }
 
         function _onOrderClick() {
-            resetTimer();
 
             if (!AccountSvc.athleteData || !AccountSvc.athleteData[0]) {
                 var opts = {};
@@ -83,13 +82,41 @@
         }
 
         function _orderItem(item, isSnack, debit) {
-            resetTimer();
+			//start a promise chain with default continue = TRUE
+			var chain = $q.when( true );
+			//test to see if item exceeds daily account limit
+			if (item.choiceValue + AccountSvc.totalCount > 5) {
+				//if exceeds, confirm user wants to override the limit and add to promise chain
+				chain = chain.then(_confirmOverride);
+			}
+			//add a final step to promise chain for processing if override = TRUE
+			chain.then(function(res) {
+				//will be true if default or user chooses override option
+				if (res) {
+					_addItemToOrder(item, isSnack, debit);
+				}
+			});
+        }
+		
+		function _confirmOverride() {
+			return $ionicPopup.confirm({
+				template: 'Selected item would exceed daily max of 5 credits.  Would you like override the limit for this order?',
+				title: 'Max 5 Credits',
+				scope: $scope,
+				okText: 'Override',
+				cancelText: 'Cancel'
+			}).then(function(res) {
+				console.log('Tapped!', res);
+				return res;
+			});
+		};
+					
+		function _addItemToOrder(item, isSnack, debit) {
+			// Clone the item that was added to cart to keep track of whether it is a snack or not. If choice is not cloned first, the snack attribute will remain even if the athlete selects it as pre/post
+			var itemClone = _.clone(item);
+			itemClone.orderType = debit;
 
-            // Clone the item that was added to cart to keep track of whether it is a snack or not. If choice is not cloned first, the snack attribute will remain even if the athlete selects it as pre/post
-            var itemClone = _.clone(item);
-            itemClone.orderType = debit;
-
-            if (isSnack) {
+			if (isSnack) {
 				if(!item.isFree) {
 					AccountSvc.monthSnacksRemaining--;
 					AccountSvc.daySnacksRemaining--;
@@ -97,8 +124,8 @@
 					AccountSvc.snackCount++;
 					AccountSvc.totalCount += item.choiceValue;
 				}
-            } else {
-                itemClone.isSnack = false;
+			} else {
+				itemClone.isSnack = false;
 				if (!item.isFree) {
 					if (debit === 'pre') AccountSvc.preCount++;
 					if (debit === 'post') AccountSvc.postCount++;
@@ -106,14 +133,13 @@
 					if (debit === 'staff') AccountSvc.staffCount++;
 					AccountSvc.totalCount += item.choiceValue;
 				}
-            }
+			}
 
-            OrderSvc.addItem(itemClone);
-            AccountSvc.updateHiddenCategories(OrderSvc.orderItems);
-        }
+			OrderSvc.addItem(itemClone);
+			AccountSvc.updateHiddenCategories(OrderSvc.orderItems);
+		}
 
         function _removeItem(index) {
-            resetTimer();
 			
 			if (!OrderSvc.orderItems[index].isFree) {
 			
@@ -146,10 +172,6 @@
 
             OrderSvc.removeItem(index);
             AccountSvc.updateHiddenCategories(OrderSvc.orderItems);
-        }
-
-        function resetTimer() {
-            vm.timeRemaining = 120;
         }
 
         function _toggleLeft() {
