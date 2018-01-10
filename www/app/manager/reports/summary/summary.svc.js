5c89ef9ab55d80a32962017f9283abe6fb9e9d94
jeffleus <jeffleus@gmail.com>
2018-01-10 10:58:38 -0800

    Change checkout summary URL to live service

--- www/app/manager/reports/summary/summary.svc.js
+++ www/app/manager/reports/summary/summary.svc.js
@@ -10,7 +10,7 @@
 		  getSummary: _getSummary
       };
 
-      var url = 'https://f5ekrwo1b6.execute-api.us-west-2.amazonaws.com/dev/report/checkouts/summary/';
+      var url = 'https://vydrs6y4b7.execute-api.us-west-2.amazonaws.com/dev/report/checkouts/summary/';
 
       return service;
 
