2ec4ba05cb67b61b61204422091b99568b257bdd
jeffleus <jeffleus@gmail.com>
2018-03-03 14:08:19 -0800

    filter http-interceptor for AWS gateway calls
    Added a test on URL to see if part of AWS services
    ‘us-west-2.amazonaws.com’)

--- www/app/core/http-interceptor.js
+++ www/app/core/http-interceptor.js
@@ -6,11 +6,17 @@
     app.factory('authInterceptor', function (AuthSvc) {
         return {
             request: function(config) {
-                return AuthSvc.getToken().then(function(token) {
-                    config.headers['Authorization'] = AuthSvc.token;
-                    return config;                    
-                });
-            }
+				if (config.url.includes('us-west-2.amazonaws.com')) {					
+					return AuthSvc.getToken().then(function(token) {
+						if (!token) {
+							console.warn('The token was returned null to the http service');
+							console.info(config.url);
+						}
+						config.headers['Authorization'] = token;
+						return config;                    
+                	});
+				} else { return config; }
+			}
         }
     });
 
