b70e03c3b8fa7c11c1325cd08d73004ce9130260
Jeff Leininger <jeffleus@gmail.com>
2018-08-08 06:55:09 -0700

    Baylor styling updates for demo

--- www/js/app.js
+++ www/js/app.js
@@ -79,7 +79,7 @@ angular.module('app', [
             url: '/studentID',
             views: {
                 'appContent': {
-                    templateUrl: 'athleteLogin/login.html',
+                    templateUrl: 'app/athleteLogin/login.html',
                     controller: 'StudentLoginCtrl as vm'
                 },
                 'sideMenu': {
