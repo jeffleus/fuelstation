664c10b13c13ef2e53eafdac22d1731780c398a4
Jeff Leininger <jeffleus@gmail.com>
2018-09-15 07:55:49 -0700

    fixed polling issues to avoid high traffic
    eliminated basic choice polling in the base mgrCtrl to avoid over-active polling causing high traffic.
    
    also chancged orderListCtrl to poll based on a controller variable, set for 15sec for now.

--- www/app/manager/manager.controller.js
+++ www/app/manager/manager.controller.js
@@ -6,8 +6,5 @@
     .controller('ManagerCtrl', function (ChoiceSvc, IonicAlertSvc) {
         var vm = this;
 
-        window.setInterval(function () {
-            ChoiceSvc.choice().get({id:1});
-        }, 30000);
     });
 })();
